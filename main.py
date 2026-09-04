49c225cf5d894a3dfb37c8e467fc28810349557b
M4RG3 <john.margaronis@gmail.com>
2026-09-03 20:46:08 -0700

    solid basic player movement

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
-
-def main():
-
+def main() -> None:
     pygame.init()
-
+    clock = pygame.time.Clock()
+    dt: float = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)   
+    
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        updatable.update(dt)
+        
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+        
         pygame.display.flip()
-
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
-    
+        dt = clock.tick(60) / 1000
+            
 
 if __name__ == "__main__":
     main()
